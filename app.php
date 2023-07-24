cbfe0ebf4b786d3c7dc868769b6c4c46e9ec3b77
Eduardo Lazzaretti <duadsal@gmail.com>
2023-07-24 20:53:30 -0300

    First class

---
+++ app.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+
+use Duadsal\PhpPoo\PessoaFisica;
+
+
+$pessoaFisica = new PessoaFisica;
+
+dump($pessoaFisica);
---
+++ src/PessoaFisica.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duadsal\PhpPoo;
+
+//Pense em uma classe como uma forma, que vai nos possibilitar criar diversos
+//objetos do tipo:
+class PessoaFisica
+{
+    //aqui fica as propriedades e métodos
+    public string $name    = "Eduardo" ;
+    public string $email   = "EduardoTeste@teste.com" ;
+    public string $phone   = "59 999999999" ;
+    public string $cpf     = "01301301212" ;
+    public int    $age     = 20;
+}
+
